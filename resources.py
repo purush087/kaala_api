e10ed14974c229b1d02d1915a9066c151aa5e507
Purushotham Reddy <purushothamreddy@Purushothams-MacBook-Pro.local>
2018-05-07 13:09:27 +0530

    modified status code for user not found

--- resources.py
+++ resources.py
@@ -74,7 +74,7 @@ class UserLogin(Resource):
                 'refresh_token': refresh_token
             })
         else:
-            return {'message': 'Wrong credentials'}
+            return {'message': 'Wrong credentials'}, 401
 
 
 class UserLogoutAccess(Resource):
